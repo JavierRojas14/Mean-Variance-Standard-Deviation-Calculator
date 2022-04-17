7c739972ebdc2a843f8aea140e4c1d60c6ce5941
MrHamburguesa <javierrojas14.jr@gmail.com>
2022-04-17 18:05:16 -0400

    Proyecto terminado

---
+++ main.py
@@ -0,0 +1,5 @@
+from mean_var_std import calculate
+
+lista_problema = [0, 1, 2, 3, 4, 5, 6, 7, 8] 
+
+print(calculate(lista_problema))
--- mean_var_std.py
+++ mean_var_std.py
@@ -1,19 +1,48 @@
 import numpy as np
+from pandas import array
 
 def calculate(list):
     if len(list) < 9:
         raise ValueError('List must contain nine numbers.')
     
     else:
+        # Función hacer matriz
         matriz = np.zeros((1, 3))
         for i in range(0, 9, 3):
             arreglo = np.array(list[i: i + 3])
             matriz = np.vstack([matriz, arreglo])
         
         matriz = matriz[1:]
+        resultados = calcular_estadisticas(matriz)
 
+        return resultados
 
-    calculations = None
-    return calculations
+def calcular_estadisticas(matriz):
+    resultado = {'mean': [], 'variance': [], 'standard deviation': [],
+                 'max': [], 'min': [], 'sum': []}
 
-calculate([1, 2,3,4,5,6,7,8,9])
+    for i in range(3):
+        if i != 2:
+            mean = np.mean(matriz, axis = i)
+            var = np.var(matriz, axis = i)
+            std = np.std(matriz, axis = i)
+            max = np.max(matriz, axis = i)
+            min = np.min(matriz, axis = i)
+            sum = np.sum(matriz, axis = i)
+        
+        else:
+            mean = np.mean(matriz)
+            var = np.var(matriz)
+            std = np.std(matriz)
+            max = np.max(matriz)
+            min = np.min(matriz)
+            sum = np.sum(matriz)
+        
+        resultado['mean'].append(mean.tolist())
+        resultado['variance'].append(var.tolist())
+        resultado['standard deviation'].append(std.tolist())
+        resultado['max'].append(max.tolist())
+        resultado['min'].append(min.tolist())
+        resultado['sum'].append(sum.tolist())
+
+    return resultado
